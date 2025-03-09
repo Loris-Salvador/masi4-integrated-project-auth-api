64713e96bced23bbb4905e01d145e1cfb37c4e54
Loris Salvador <dev.loris.salvador@gmail.com>
2025-03-09 18:45:58 +0100

    rework config for mail

--- src/main/java/be/hepl/authapi/infrastructure/service/EmailServiceImpl.java
+++ src/main/java/be/hepl/authapi/infrastructure/service/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package be.hepl.authapi.infrastructure.service;
 
 import be.hepl.authapi.application.service.EmailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,16 @@ public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     public EmailServiceImpl(JavaMailSender emailSender) {
         this.emailSender = emailSender;
     }
 
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("loris3salvador@gmail.com");
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
